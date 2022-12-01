fb819ffc5d159751ad72c5ef954cbb536deac4ad
Thomas Benoit <thomas.benoit@ifremer.fr>
2022-12-01 17:44:46 +0100

    solve (2) ?

--- R/compnum_preprocessing.R
+++ R/compnum_preprocessing.R
@@ -1,8 +1,5 @@
 compnum_preprocessing <- function(data){
   compnum <- data[,4:ncol(data)]
-  compnum$X <- data$X
-  compnum <- compnum[, c("X", names(compnum)[names(compnum) != "X"])]
   compnum <- textshape::column_to_rownames(compnum,"Point")
   return(compnum)
 }
-
